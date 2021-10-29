b216a797a162b159179e9251d0c6bef4b489c752
Otavio Cantareli <otavio_cantareli@outlook.com>
2021-10-29 17:54:15 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/Header';
 import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 class App extends React.Component {
   render() {
@@ -10,9 +9,7 @@ class App extends React.Component {
     return (
       <>
         <Header />
-        <SolarSystem>
-          <Title headline="Planetas" />
-        </SolarSystem>
+        <SolarSystem />
         <Missions />
       </>
     );
